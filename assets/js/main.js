1839a8e2c5f200f8e28359f307d6e4f8fdba6e65
GuilhermeSalles <guibaltazarvs@gmail.com>
2025-05-20 15:09:11 -0300

    imagens atualizadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,7 +109,7 @@ sr.reveal(`.home__img`, {delay: 300})
 sr.reveal(`.home__social`, {delay: 200})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
-sr.reveal(`.steps__card, .product__card, .questions__group, .footer, .card__content`,{interval: 100})
+sr.reveal(`.steps__card, .product__card, .questions__group, .card__content`,{interval: 100})
 sr.reveal(`.section__title-center`, {interval: 100});
 sr.reveal(`.feature-card, .about-differences__text`, {interval: 100});
 sr.reveal(`.about-cta__content, .about-differences__content`, {origin: 'bottom'});
